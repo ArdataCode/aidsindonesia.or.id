0e13dd0bd062dd46695c9eae8ec871b0143cf0d0
ArdataCode <ardata.indonesia@gmail.com>
2024-05-17 15:13:08 +0700

    mit

---
+++ malcare-waf.php
@@ -0,0 +1,8 @@
+<?php
+// Please validate auto_prepend_file setting before removing this file
+
+if (file_exists('/home/ujianori/aidsindonesia.or.id/wp-content/plugins/malcare-security/protect/prepend/ignitor.php')) {
+	define("MCDATAPATH", '/home/ujianori/aidsindonesia.or.id/wp-content/mc_data/');
+	define("MCCONFKEY", '1f93177710e5d90d847e5f3fd9928c90');
+	include_once('/home/ujianori/aidsindonesia.or.id/wp-content/plugins/malcare-security/protect/prepend/ignitor.php');
+}
---
+++ wp-salt.php
@@ -0,0 +1,17 @@
+<?php
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ */
+define('AUTH_KEY',         '3GIJDQWBzJb8vbPtjwNgwnEBRezgFvow9w745pxd5BEFdmUvDqyG98Fg045VVM5K');
+define('SECURE_AUTH_KEY',  '0BD05YD3qgKvDGCCeUBUDdSaKBRaeabJYrEU66tSsdFVCI3msts2KeXYYuP6sP0w');
+define('LOGGED_IN_KEY',    'MtQ55cvCFH6Q3Fm9ryVvP6xCx16vMFVf5f7h5eHye9rPvxjo5goa5Tdcz4hp5raq');
+define('NONCE_KEY',        '64Q59YMebbTeSDrodDnKYWrCRn8Da0VTmxXvNgUGKz1rrvFMM9dFXwmH4rLb3Wmw');
+define('AUTH_SALT',        'o2HTR1WpK4H6ijvWbh4bMtzQFgH6YHh9j8DY4R0dfniE83uEoDMU0qwRRpEMRADN');
+define('SECURE_AUTH_SALT', '9AxfRb6dRFqqqpI1I3s1KYTAp95s5PI4KTbBeR1612FL1HqYbPAnIMygpRKvQd33');
+define('LOGGED_IN_SALT',   'cA29xwPIGS43nsH7e1yvvsR2Qbp7AcYx0rWCYxgzTQhb9e87fXSi53vvEeKgDWEg');
+define('NONCE_SALT',       'UF9baHs5iK2DfKUHv52An4nT0wW3A9uFoHEXuoSgq3qiwTzheLNcp9XCG9DJs8MC');
